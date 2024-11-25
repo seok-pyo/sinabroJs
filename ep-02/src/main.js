573bb0f0b5a7c424a4c4d6be970495c798e85f8a
hongseokpyo <hongseokpyou@gmail.com>
2024-11-25 21:21:21 +0900

    html, css setting

--- ep-02/src/main.js
+++ ep-02/src/main.js
@@ -1,12 +1,23 @@
 // https://learnwitheunjae.dev/api/sinabro-js/ecommerce
 // api
 
-async function main() {
-  const response = await fetch(
-    'https://learnwitheunjae.dev/api/sinabro-js/ecommerce'
-  );
-  const products = await response.json();
+import test from './test.json?raw'; // ?raw Vite이 제공하는 기능
+
+async function getProducts() {
+  if (process.env.NODE_ENV === 'development') {
+    return JSON.parse(test);
+  } else {
+    const response = await fetch(
+      'https://learnwitheunjae.dev/api/sinabro-js/ecommerce'
+    );
+    const products = await response.json();
+  }
   console.log(products);
+}
+
+async function main() {
+  // console.log(process.env.NODE_ENV);
+  const products = await getProducts();
 
   document.querySelector('#products').innerHTML = products
     .map(
@@ -14,6 +25,14 @@ async function main() {
       <div class="product">
         <img src="${product.images[0]}" alt="Image of ${product.name}" />
         <p>${product.name}</p>
+        <div class="flex items-center justify-between">
+          <span>Price: ${product.regularPrice}</span>
+          <div>
+            <button type="button" class="bg-green-300 hover:bg-green-600 py-1 px-3 rounded-full">-</button>
+            <span class="hidden text-green-700">3</span>
+            <button type="button" class="bg-green-300 hover:bg-green-600 py-1 px-3 rounded-full">+</button>
+          </div>
+        </div>
       </div>
     `
     )
