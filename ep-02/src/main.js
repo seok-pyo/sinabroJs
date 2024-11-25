09d2567d3fd72d548f65bd871bc47f35061fd772
hongseokpyo <hongseokpyou@gmail.com>
2024-11-25 20:47:02 +0900

    rendering images

--- ep-02/src/main.js
+++ ep-02/src/main.js
@@ -5,8 +5,19 @@ async function main() {
   const response = await fetch(
     'https://learnwitheunjae.dev/api/sinabro-js/ecommerce'
   );
-  const json = await response.json();
-  console.log('json', json);
+  const products = await response.json();
+  console.log(products);
+
+  document.querySelector('#products').innerHTML = products
+    .map(
+      (product) => `
+      <div class="product">
+        <img src="${product.images[0]}" alt="Image of ${product.name}" />
+        <p>${product.name}</p>
+      </div>
+    `
+    )
+    .join('');
 }
 
 main();
