0758788096e2ed962b6d26bb4edc39a8d2dd1ef6
hongseokpyo <hongseokpyou@gmail.com>
2024-11-26 17:34:28 +0900

    sum count

--- ep-02/src/main.js
+++ ep-02/src/main.js
@@ -10,26 +10,26 @@ async function getProducts() {
     const response = await fetch(
       'https://learnwitheunjae.dev/api/sinabro-js/ecommerce'
     );
-    const products = await response.json();
+    return await response.json();
   }
-  console.log(products);
 }
 
 async function main() {
   // console.log(process.env.NODE_ENV);
   const products = await getProducts();
+  const countMap = {};
 
   document.querySelector('#products').innerHTML = products
     .map(
-      (product) => `
-      <div class="product">
+      (product, index) => `
+      <div class="product" data-product-id="${product.id}" data-product-index="${index}">
         <img src="${product.images[0]}" alt="Image of ${product.name}" />
         <p>${product.name}</p>
         <div class="flex items-center justify-between">
           <span>Price: ${product.regularPrice}</span>
           <div>
             <button type="button" class="bg-green-300 hover:bg-green-600 py-1 px-3 rounded-full btn-decrease">-</button>
-            <span class="hidden text-green-700">3</span>
+            <span class="cart-count text-green-700"></span>
             <button type="button" class="bg-green-300 hover:bg-green-600 py-1 px-3 rounded-full btn-increase">+</button>
           </div>
         </div>
@@ -37,6 +37,40 @@ async function main() {
     `
     )
     .join('');
+
+  document.querySelector('#products').addEventListener('click', (event) => {
+    const targetElement = event.target;
+    const productElement = findElement(targetElement, '.product');
+    const productId = productElement.getAttribute('data-product-id');
+    const productIndex = productElement.getAttribute('data-product-index');
+    const product = products[productIndex];
+
+    if (
+      targetElement.matches('.btn-decrease') ||
+      targetElement.matches('.btn-increase')
+    ) {
+      if (countMap[productId] === undefined) {
+        countMap[productId] = 0;
+      }
+      if (targetElement.matches('.btn-decrease') && countMap[productId] > 0) {
+        countMap[productId] -= 1;
+      } else if (targetElement.matches('.btn-increase')) {
+        countMap[productId] += 1;
+      }
+      const cartCount = productElement.querySelector('.cart-count');
+      cartCount.innerHTML = countMap[productId];
+      if (countMap[productId] === 0) cartCount.innerHTML = '';
+
+      document.querySelector('.totalCount').innerHTML = sumCounts(countMap);
+    }
+  });
+}
+
+function sumCounts(countMap) {
+  return Object.values(countMap).reduce((acc, cur) => {
+    acc += cur;
+    return acc;
+  }, 0);
 }
 
 function findElement(startingElement, selector) {
@@ -49,17 +83,6 @@ function findElement(startingElement, selector) {
   }
   return null;
 }
-
-document.querySelector('#products').addEventListener('click', (event) => {
-  console.log(event.target);
-  const targetElement = event.target;
-  const producetElement = findElement(targetElement, '.product');
-  if (targetElement.matches('.btn-decrease')) {
-    console.log('decrease');
-  } else if (targetElement.matches('.btn-increase')) {
-    console.log('increase');
-  }
-});
 
 /*
 Advanced event handling
